c33f4b306b9da6d7c49f7eb47cae23697e544934
twsswt <timothy.storer@glasgow.ac.uk>
2017-01-11 13:59:48 +0000

    Fixed defect in CompletedTask.__str__ due to potential to try and format None as number for finish tick.

--- theatre_ag/task.py
+++ theatre_ag/task.py
@@ -23,19 +23,28 @@ class AllocatedTask(object):
 class CompletedTask(object):
 
     def __init__(self, func, parent, start_tick):
-        self.parent = parent
-        self.sub_tasks = list()
-
         self.func = func
+        self.parent = parent
         self.start_tick = start_tick
+
         self.finish_tick = None
+        self.sub_tasks = list()
 
     def append_sub_task(self, sub_task):
         self.sub_tasks.append(sub_task)
         pass
 
     def __str__(self):
-        return "%s(%d->%d)" % (self.func.func_name, self.start_tick, self.finish_tick)
+        return "%s(%d->%s)" % (self.func.func_name, self.start_tick, str(self.finish_tick))
 
     def __repr__(self):
         return self.__str__()
+
+    def __eq__(self, other):
+        if not isinstance(other, CompletedTask):
+            return NotImplemented
+        else:
+            return self.func == other.func and \
+                   self.parent == other.parent and \
+                   self.start_tick == other.start_tick and \
+                   self.finish_tick == other.finish_tick
