895fc175fa2148844afde6ef1da34555125e0766
twsswt <timothy.storer@glasgow.ac.uk>
2017-02-21 19:28:48 +0000

    Added methods for shutting down a whole cast.

--- theatre_ag/cast.py
+++ theatre_ag/cast.py
@@ -19,6 +19,25 @@ class Cast(object):
         for actor in self.members:
             actor.start()
 
+    def shutdown(self):
+        """
+        Ends the performance of the cast by first initiating the shutdown of all member actors and then waiting for
+        their termination.  This method can be safely called when the cast's clock is executed in a separate thread to
+        the invocation.  Otherwise the initiate shutdown should be called first, then a clock tick issued, followed by
+        the wait for shutdown.
+        :return:
+        """
+        self.initiate_shutdown()
+        self.wait_for_shutdown()
+
+    def initiate_shutdown(self):
+        for actor in self.members:
+            actor.initiate_shutdown()
+
+    def wait_for_shutdown(self):
+        for actor in self.members:
+            actor.wait_for_shutdown()
+
     @property
     def last_tick(self):
 
