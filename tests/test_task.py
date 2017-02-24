84023497b8ac7d878408846aa0b98a25265e35dc
twsswt <timothy.storer@glasgow.ac.uk>
2017-02-24 10:48:35 +0000

    Added properties to tasks for finding last significant commits.

---
+++ tests/test_task.py
@@ -0,0 +1,134 @@
+import unittest
+
+from theatre_ag.task import Task
+from theatre_ag.workflow import Idling
+
+
+class TaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.task = Task(None, None)
+
+    def test_last_non_idling_tick_a(self):
+        """
+        The main task has been initiated.
+        """
+        self.task.initiate(1)
+        self.assertEquals(1, self.task.last_non_idling_tick)
+
+    def test_last_non_idling_tick_b(self):
+        """
+        The main task has completed.
+        """
+        self.task.initiate(1)
+        self.task.complete(2)
+        self.assertEquals(2, self.task.last_non_idling_tick)
+
+    def test_last_non_idling_tick_c(self):
+        """
+        The main task has been initiated.
+        One non-idling sub task has been initiated.
+        """
+        self.task.initiate(1)
+        sub_task = self.task.append_sub_task(None, None, ())
+        sub_task.initiate(2)
+        self.assertEquals(2, self.task.last_non_idling_tick)
+
+    def test_last_non_idling_tick_d(self):
+        """
+        The main task has been initiated.
+        One non-idling sub task has completed.
+        """
+        self.task.initiate(1)
+        sub_task = self.task.append_sub_task(None, None, ())
+        sub_task.initiate(2)
+        sub_task.complete(3)
+        self.assertEquals(3, self.task.last_non_idling_tick)
+
+    def test_last_non_idling_tick_e(self):
+        """
+        The main task has been completed.
+        One non-idling sub task has completed.
+        """
+        self.task.initiate(1)
+        sub_task = self.task.append_sub_task(None, None, ())
+        sub_task.initiate(2)
+        sub_task.complete(3)
+        self.task.complete(4)
+        self.assertEquals(4, self.task.last_non_idling_tick)
+
+    def test_last_non_idling_tick_f(self):
+        """
+        The main task has been initiated.
+        One idling sub task has initiated.
+        """
+        self.task.initiate(1)
+        sub_task = self.task.append_sub_task(Idling, Idling.idle, ())
+        sub_task.initiate(2)
+        sub_task.complete(3)
+        self.assertEquals(1, self.task.last_non_idling_tick)
+
+    def test_last_non_idling_tick_f(self):
+        """
+        The main task has been completed.
+        One idling sub task has completed.
+        """
+        self.task.initiate(1)
+        sub_task = self.task.append_sub_task(Idling, Idling.idle, ())
+        sub_task.initiate(2)
+        sub_task.complete(3)
+        self.task.complete(4)
+        self.assertEquals(4, self.task.last_non_idling_tick)
+
+    def test_last_non_idling_tick_g(self):
+        """
+        The main task has been initiated.
+        One idling sub task has completed.
+        One sub task has been initiated
+        """
+        self.task.initiate(1)
+        idling_sub_task = self.task.append_sub_task(Idling, Idling.idle, ())
+        idling_sub_task.initiate(2)
+        idling_sub_task.complete(3)
+
+        sub_task = self.task.append_sub_task(None, None, ())
+        sub_task.initiate(4)
+        self.assertEquals(4, self.task.last_non_idling_tick)
+
+    def test_last_non_idling_tick_h(self):
+        """
+        The main task has been initiated.
+        One sub task has been completed.
+        One idling sub task has completed.
+        """
+        self.task.initiate(1)
+        sub_task = self.task.append_sub_task(None, None, ())
+        sub_task.initiate(2)
+        sub_task.complete(3)
+
+        idling_sub_task = self.task.append_sub_task(Idling, Idling.idle, ())
+        idling_sub_task.initiate(4)
+        idling_sub_task.complete(5)
+
+        self.assertEquals(3, self.task.last_non_idling_tick)
+
+    def test_last_non_idling_tick_i(self):
+        """
+        The main task has been initiated.
+        One sub task has been completed.
+        One idling sub task  of the sub task has completed.
+        """
+        self.task.initiate(1)
+        sub_task = self.task.append_sub_task(None, None, ())
+        sub_task.initiate(2)
+        sub_task.complete(3)
+
+        idling_sub_task = sub_task.append_sub_task(Idling, Idling.idle, ())
+        idling_sub_task.initiate(4)
+        idling_sub_task.complete(5)
+
+        self.assertEquals(3, self.task.last_non_idling_tick)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- theatre_ag/task.py
+++ theatre_ag/task.py
@@ -1,6 +1,7 @@
 """
 @author twsswt
 """
+from workflow import Idling
 
 
 class Task(object):
@@ -22,7 +23,7 @@ class Task(object):
     def initiate(self, start_tick):
         self.start_tick = start_tick
 
-    def append_sub_task(self, workflow, entry_point, args):
+    def append_sub_task(self, workflow, entry_point, args=()):
         sub_task = Task(workflow, entry_point, args, parent=self)
         self.sub_tasks.append(sub_task)
         return sub_task
@@ -37,6 +38,24 @@ class Task(object):
     @property
     def completed(self):
         return self.finish_tick is not None
+
+    @property
+    def non_idling_sub_tasks(self):
+        return filter(lambda t: t.workflow is not Idling, self.sub_tasks)
+
+    @property
+    def last_non_idling_sub_task(self):
+        return None if len(self.non_idling_sub_tasks) == 0 else self.non_idling_sub_tasks[-1]
+
+    @property
+    def last_non_idling_tick(self):
+        if self.completed:
+            return self.finish_tick
+        else:
+            if self.last_non_idling_sub_task is None:
+                return self.start_tick
+            else:
+                return self.last_non_idling_sub_task.last_non_idling_tick
 
     def __repr__(self):
 
