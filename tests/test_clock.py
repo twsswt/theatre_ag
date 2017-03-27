9e1584c4177fc072d96f89e231e834e39c9119d3
twsswt <timothy.storer@glasgow.ac.uk>
2017-03-27 20:58:56 +0100

    Removed unintentionally added test (for experimental code - should be in a branch)

--- tests/test_clock.py
+++ tests/test_clock.py
@@ -24,12 +24,6 @@ class ClockTestCase(unittest.TestCase):
         self.tick_listener.waiting_for_tick.wait.assert_called_once_with()
         self.tick_listener.notify_new_tick.assert_called_once_with()
 
-    def test_clock_synchronization(self):
-
-        peer_clock = SynchronizingClock(max_ticks=20)
-
-        self.clock.synchronize_on(peer_clock, granularity=1)
-
 
 if __name__ == '__main__':
     unittest.main()
